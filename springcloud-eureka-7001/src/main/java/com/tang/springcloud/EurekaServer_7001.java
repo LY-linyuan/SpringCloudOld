d20dcef7699ea2af5107f21bb05817881406aac8
LY-linyuan <3113714035@qq.com>
2022-10-17 17:57:09 +0800

    SpringCloudOld尝试学习中---------

---
+++ springcloud-eureka-7001/src/main/java/com/tang/springcloud/EurekaServer_7001.java
@@ -0,0 +1,18 @@
+package com.tang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author 临渊
+ * @Date 2022-10-17 17:50
+ */
+
+@SpringBootApplication
+@EnableEurekaServer   // @EnableEurekaServer  服务端的启动类, 可以注册别人注册进来
+public class EurekaServer_7001{
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class, args);
+    }
+}
